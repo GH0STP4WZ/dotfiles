1875e13b13206e6a39c1ea5dff63fc9a8079fbf3
R3DGH0STP4WZ <gh0st13@gh0stp4wz.is-a.dev>
2025-10-30 11:00:10 +0100

    hyprland configs

---
+++ .config/hypr/scripts/snapshot.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+outputDir="$HOME/Pictures/Screenshots/"
+outputFile="snapshot_$(date +%Y-%m-%d_%H-%M-%S).png"
+outputPath="$outputDir/$outputFile"
+mkdir -p "$outputDir"
+mode=${1:-area}
+case "$mode" in
+active)
+    command="grimblast copysave active $outputPath"
+    ;;
+output)
+    command="grimblast copysave output $outputPath"
+    ;;
+area)
+    command="grimblast copysave area $outputPath"
+    ;;
+*)
+    echo "Invalid option: $mode"
+    echo "Usage: $0 {active|output|area}"
+    exit 1
+    ;;
+esac
+if eval "$command"; then
+    recentFile=$(find "$outputDir" -name 'snapshot_*.png' -printf '%T+ %p\n' | sort -r | head -n 1 | cut -d' ' -f2-)
+    # Notify with the actual screenshot as thumbnail + buttons
+    action=$(notify-send "Grimblast" "Screenshot Saved: $recentFile" \
+        -i "$recentFile" \
+        -a "Grimblast" \
+        -t 7000 \
+        -u normal \
+        --action="view=View Image" \
+        --action="markup=Markup Image")
+
+    # Execute based on the action chosen
+    case "$action" in
+        view)
+            xdg-open "$recentFile"
+            ;;
+        markup)
+            swappy -f "$recentFile"
+            ;;
+    esac
+fi
